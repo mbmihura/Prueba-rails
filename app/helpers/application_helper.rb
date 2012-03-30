0192956e56f5850a08fec9a36a577b942f87fd00
Martin Mihura <mbmihura@gmail.com>
2012-03-29 21:13:18 -0300

    Agrega Ajax.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def hidden_div_if (condition, atrributes = {}, &block)
+		if condition
+			atrributes["style"] = "display: none"
+		end
+		content_tag("div", atrributes, &block)
+	end
 end
--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -18,10 +18,12 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, product_id: products(:ruby).id
+      xhr :post, :create, product_id: products(:ruby).id
     end
-
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
   end
 
   test "should show line_item" do
